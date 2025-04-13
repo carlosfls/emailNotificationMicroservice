7b402d4a07da0ac22bffe11f296f8c5edd8a78fc
carlos <carlosfls54@gmail.com>
2025-04-13 17:51:38 -0300

    refactor

--- src/main/java/com/carlosacademic/emailnotificationmicroservice/ProductCreatedEventHandler.java
+++ src/main/java/com/carlosacademic/emailnotificationmicroservice/ProductCreatedEventHandler.java
@@ -1,16 +1,31 @@
 package com.carlosacademic.emailnotificationmicroservice;
 
+import com.carlosacademic.emailnotificationmicroservice.errors.NonRetryableException;
 import com.carlosacademic.producteventscore.ProductCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @KafkaListener(topics = "product-created-events-topic")
 public class ProductCreatedEventHandler {
 
     @KafkaHandler
-    public void handle(ProductCreatedEvent event){
-        System.out.println("Event received!!! "+ event.title());
+    public void handle(@Payload ProductCreatedEvent event,
+                       @Header(name = "messageId") String messageId) {
+        log.info("Message received!!!");
+        validate(event);
+        log.info("Received messageId: {}, content {}",messageId, event);
     }
+
+    private void validate(ProductCreatedEvent event) throws NonRetryableException {
+        if (event.title().isBlank()) {
+            throw new NonRetryableException("Product name is empty!!");
+        }
+    }
+
 }
--- src/main/java/com/carlosacademic/emailnotificationmicroservice/config/KafkaConfig.java
+++ src/main/java/com/carlosacademic/emailnotificationmicroservice/config/KafkaConfig.java
@@ -1,9 +1,7 @@
 package com.carlosacademic.emailnotificationmicroservice.config;
 
 import com.carlosacademic.emailnotificationmicroservice.errors.NonRetryableException;
-import com.carlosacademic.emailnotificationmicroservice.errors.RetryableException;
 import lombok.RequiredArgsConstructor;
-import lombok.val;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
@@ -34,42 +32,45 @@ public class KafkaConfig {
     final Environment environment;
 
     @Bean
-    ConsumerFactory<String, Object> consumerFactory(){
+    ConsumerFactory<String, Object> createConsumerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"));
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(JsonDeserializer.TRUSTED_PACKAGES, environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
+        //error handling
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
         config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
-        config.put(JsonDeserializer.TRUSTED_PACKAGES, environment.getProperty("spring.kafka.consumer.properties.spring.json.trusted.packages"));
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"));
 
         return new DefaultKafkaConsumerFactory<>(config);
     }
 
     @Bean
     ConcurrentKafkaListenerContainerFactory<String, Object> concurrentKafkaListenerContainerFactory(
-            ConsumerFactory<String, Object> consumerFactory, KafkaTemplate<String, Object> kafkaTemplate){
+            ConsumerFactory<String, Object> consumerFactory,
+            KafkaTemplate<String, Object> kafkaTemplate) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 
-        val fixedBackOff = new FixedBackOff(5000, 3);
+        //error handling
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate),
+                new FixedBackOff(5000,1));
 
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(new DeadLetterPublishingRecoverer(kafkaTemplate), fixedBackOff);
         errorHandler.addNotRetryableExceptions(NonRetryableException.class);
-        errorHandler.addRetryableExceptions(RetryableException.class);
 
-        ConcurrentKafkaListenerContainerFactory<String,Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
         factory.setCommonErrorHandler(errorHandler);
 
         return factory;
     }
 
+    //Template to send to dead letter topic
     @Bean
-    KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory){
+    KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
     }
 
     @Bean
-    ProducerFactory<String,Object> producerFactory(){
+    ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> config = new HashMap<>();
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getProperty("spring.kafka.consumer.bootstrap-servers"));
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
