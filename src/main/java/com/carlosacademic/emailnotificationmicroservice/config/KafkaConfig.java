ac3d36b8e9a35322d484c13b6f7fc5d43b02db1a
carlos <carlosfls54@gmail.com>
2025-04-14 10:49:25 -0300

    refactor

--- src/main/java/com/carlosacademic/emailnotificationmicroservice/config/KafkaConfig.java
+++ src/main/java/com/carlosacademic/emailnotificationmicroservice/config/KafkaConfig.java
@@ -9,6 +9,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
@@ -26,6 +27,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@EnableKafka
 @RequiredArgsConstructor
 public class KafkaConfig {
 
